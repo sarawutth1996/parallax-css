3ac4511a6569489ee1afa9d1e1c472c0dcbd38f2
sarawutth1996 <sarawut_aum-1145@hotmail.com>
2023-05-16 15:49:41 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'parallex_scrolling_cover';
+  scrollValue = 0;
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll(event: Event) {
+    this.scrollValue = window.pageYOffset;
+  }
 }
